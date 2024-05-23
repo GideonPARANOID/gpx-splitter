1b62c2190cbbfc0e319a40e5ee8b89081b11d92f
Gideon MW Jones <gideon.jones@dazn.com>
2024-05-23 22:24:03 +0100

    GH Pages, distance split, tests (#2)
    * Add version of the tool for use in the browser.
    * Host with GH Pages.
    * Add option for splitting files based on distance, not just points.
    * Add tests.

---
+++ build.ts
@@ -0,0 +1,38 @@
+import * as esbuild from 'esbuild';
+import { execSync } from 'child_process';
+import tsConfigJson from './tsconfig.json';
+import packageJson from './package.json';
+
+const outDir = tsConfigJson.compilerOptions.outDir;
+
+const build = async () => {
+  try {
+    // cdns for fast-xml-parser don't seem to include the required builder component
+    const fastXmlParserVersion = packageJson.dependencies['fast-xml-parser'];
+    const commands = [
+      'yarn tsc --noEmit',
+      `rm -rf ${outDir}`,
+      `mkdir -p ${outDir}/lib`,
+      `cp src/*.html ${outDir}/`,
+
+      `curl https://raw.githubusercontent.com/NaturalIntelligence/fast-xml-parser/v${fastXmlParserVersion}/lib/fxbuilder.min.js > ${outDir}/lib/fxbuilder.min.js`,
+      ,
+      `curl https://raw.githubusercontent.com/NaturalIntelligence/fast-xml-parser/v${fastXmlParserVersion}/lib/fxparser.min.js > ${outDir}/lib/fxparser.min.js`,
+    ];
+
+    commands.forEach((command) => execSync(command));
+  } catch (error) {
+    console.error(error.stdout.toString(), error.stderr.toString());
+    process.exit(error.status);
+  }
+
+  const context = await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    minify: true,
+    target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
+    outfile: `${outDir}/app.js`,
+  });
+};
+
+void build();
---
+++ src/index.ts
@@ -0,0 +1,73 @@
+import { split } from './convert';
+import type { SplitRoute, RouteMetadata, SplitMethod } from './types';
+import { metersToKilometers } from './utils';
+
+const gpx = {
+  mimeType: 'application/gpx+xml',
+  suffix: '.gpx',
+};
+
+addEventListener('submit', async (event): Promise<void> => {
+  event.preventDefault();
+  const formData = new FormData(event.target as HTMLFormElement);
+
+  console.log('event', event, formData);
+
+  const input = {
+    file: formData.get('gpx') as File,
+    parts: parseInt(formData.get('parts') as string, 10),
+    method: formData.get('method') as SplitMethod,
+  };
+
+  console.log(input);
+
+  try {
+    const result = await split(await input.file.text(), input.parts, input.method);
+
+    setInfo(result.metadata);
+    setOutput(input.file.name.replace(gpx.suffix, ''), result.parts);
+  } catch (error) {
+    setError('Error encountered during splitting');
+    throw error;
+  }
+});
+
+const setError = (error: string): void => {
+  document.getElementById('info-table-body').innerHTML = `
+<tr>
+  <td colspan="2">${error}</td>
+</tr>`;
+
+  document.getElementById('output-table-body').innerHTML = `
+<tr>
+  <td colspan="4">${error}</td>
+</tr>`;
+};
+
+const setInfo = (metadata: RouteMetadata): void => {
+  document.getElementById('info-table-body').innerHTML = `
+<tr>
+  <td>${metersToKilometers(metadata.lengthMeters)}</td>
+  <td>${metadata.pointsCount}</td>
+</tr>`;
+};
+
+const setOutput = (inputFileName: string, parts: SplitRoute['parts']): void => {
+  const rows = parts.map((content, index) => {
+    const file = new File([content.route], `${inputFileName}-${index + 1}${gpx.suffix}`, {
+      type: gpx.mimeType,
+    });
+
+    return createOutputRow(file, index + 1, content.metadata);
+  });
+
+  const outputTableBody = (document.getElementById('output-table-body').innerHTML = rows.join(''));
+};
+
+const createOutputRow = (file: File, index: number, metadata: RouteMetadata): string => `
+<tr>
+  <th scope="row">${index}</th>
+  <td>${metersToKilometers(metadata.lengthMeters)}</td>
+  <td>${metadata.pointsCount}</td>
+  <td><a href="${URL.createObjectURL(file)}" download="${file.name}">Download</a></td>
+</tr>`;
